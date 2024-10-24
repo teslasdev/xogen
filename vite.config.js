e0e01a86e99419322d03c45d12234a82bc037dad
teslasdev <teslasdeveloper@gmail.com>
2024-10-24 08:32:48 +0100

    puhsed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [svgr(), react(), tsconfigPaths()],
+  plugins: [svgr(), react()],
   resolve: {
     alias: {
       "./runtimeConfig": "./runtimeConfig.browser",
