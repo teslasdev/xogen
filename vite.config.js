b9713c7b6d3968a3b520e7aac2556d9934c66141
teslasdev <teslasdeveloper@gmail.com>
2024-10-24 08:27:03 +0100

    puhsed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'; // Import the plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() , svgr()],
+  
 })
